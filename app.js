be2daed754676192fea11a05e8fb1d14d9f24a25
nienni <jiabonm@gmail.com>
2019-09-07 22:25:51 +0800

    Add route

--- app.js
+++ app.js
@@ -24,9 +24,46 @@ db.once('open', () => {
 const Restaurant = require('./models/restaurant.js')
 
 //route
+//首頁
 app.get('/', (req, res) => {
   res.send('hello')
 })
+
+//列出全部餐聽
+app.get('/restaurants', (req, res) => {
+  res.send('列出所有餐廳')
+})
+
+//新增一筆餐廳頁面
+app.get('/restaurants/new', (req, res) => {
+  res.send('新增餐廳頁面')
+})
+
+//顯示一筆餐廳
+app.get('/restaurant/:_id', (req, res) => {
+  res.send('顯示一筆餐廳')
+})
+
+//新增一筆餐廳
+app.post('/restaurants/new', (req, res) => {
+  res.send('新增餐廳')
+})
+
+//編輯餐廳介面
+app.get('/restaurant/:_id/edit', (req, res) => {
+  res.send('編輯餐廳')
+})
+
+//修改餐廳
+app.post('/restaurants/:_id/edit', (req, res) => {
+  res.send('修改餐聽')
+})
+
+//刪除餐廳
+app.post('/restaurant/:_id/delete', (req, res) => {
+  res.send('刪除餐廳')
+})
+
 
 //listen
 app.listen(3000, () => {
