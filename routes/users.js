a462044c2c0347c3dd5c226bd82822c8a1a36143
nienni <jiabonm@gmail.com>
2019-09-15 21:34:56 +0800

    Add logout feture

--- routes/users.js
+++ routes/users.js
@@ -50,7 +50,8 @@ router.post('/register', (req, res) => {
 
 //logout
 router.get('/logout', (req, res) => {
-  res.send('logout')
+  req.logout()
+  res.redirect('/users/login')
 })
 
 module.exports = router
